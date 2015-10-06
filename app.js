3edb6ae5297c80397f7d589216c18ab1fbb53768
JASMINE BROWN <jasmineiona@JASMINEs-MacBook-Pro.local>
2015-10-06 14:53:11 -0700

    salmon working

--- app.js
+++ app.js
@@ -1,7 +1,40 @@
-var pikePlace = {
-	location: 'pikePlaceMarket',
-	min CustHour: 20,
-	max CustHour: 50,
-	average cookies cust: 5,
-	randCustHour: function()
-}
+var CookieStand = function(place, minCustHour, maxCustHour, avgCookieCust){
+	this.place = place;
+	this.minCustHour = minCustHour;
+	this.maxCustHour = minCustHour;
+	this.avgCookieCust = avgCookieCust;
+	this.cookiesByHourList = [];
+
+
+	this.randCustHour = function(){
+		return math.floor(math.random() * (this.maxCustHour - this.minCustHour + i) + this.minCustHour);
+	};
+
+	this.totalCookiesHour = function(){
+		return math.floor(this.randCustHour() + this.avgCookieCust);
+	};
+
+	this.totalCookiesDay = function(){
+		var dailyCookies = 0;
+		for (var i = 0; i < 0; i ++);
+	};
+
+this.cookiesByHourList.push(this.totalCookiesHour());
+dailyCookies = dailyCookies + this.cookiesByHourList[i];
+
+console.log("24-hour time:"(i + 10));
+console.log ("running totals of cookies:" + dailyCookies);
+console.log(this.cookiesByHourList);
+console.log(this.place + ":cookies this hour:" + this.cookiesByHourList[i]);
+console.log("++++++++++++++++++++++++++++++++");
+
+
+
+var pikePlace = new CookieStand('Pike Place Market', 17, 88, 5.2);
+var seaTac = new CookieStand('SeaTac', 6, 44, 1.2);
+var southCenter = new CookieStand('Southcenter Mall', 11, 38, 1.9);
+var bellevueSquare = new CookieStand('Bellevue Square', 20, 48, 3.3);
+var Alki = new CookieStand('Alki Beach', 3, 24, 2.6);
+
+return dailyCookies;
+};
